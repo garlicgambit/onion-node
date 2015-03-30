035eb9178d10cb978434c24a137e7747a0c4ad03
Jules Mercier <jmercier@openmailbox.org>
2015-03-30 22:27:00 +0000

    Added apt-install.sh file

---
+++ apt-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fetch updates
+apt-get update;
+
+# Install updates
+apt-get upgrade -y;
+
+# Install Tor
+apt-get install -y tor;
+
+# Install other software
+apt-get install -y gnupg-curl macchanger unattended-upgrades;
+
+# Install bitcoin dependencies
+apt-get install -y autoconf build-essential libboost-chrono-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libssl-dev;
+
+# Install tlsdate dependencies
+apt-get install -y autoconf autotools-dev build-essential fakeroot libevent-dev libssl-dev libtool pkg-config;
