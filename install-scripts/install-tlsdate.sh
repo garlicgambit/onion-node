d97b52e6d0cddfec752c2eb032b4bebc9be2489a
Jules Mercier <jmercier@openmailbox.org>
2015-04-11 23:19:39 +0000

    Changed install-tlsdate.sh added run as root check

--- install-scripts/install-tlsdate.sh
+++ install-scripts/install-tlsdate.sh
@@ -7,6 +7,12 @@ SRCDIR=/usr/local/src/tlsdate;
 TLSDATEURL=https://www.github.com/ioerror/tlsdate.git;
 
 
+# Only run as root
+if [[ "$(id -u)" != "0" ]]; then
+  echo "ERROR: Must be run as root...exiting script";
+  exit 0;
+fi
+
 # Download latest version from github.com
 echo "Download latest version from "$TLSDATEURL"";
 
