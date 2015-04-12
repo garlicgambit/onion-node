4663124517a1a07a22dc2aa22fbe5264751df541
Jules Mercier <jmercier@openmailbox.org>
2015-04-12 04:29:21 +0000

    Added random sleep gpg lookup download-gpg-keys.sh
    This is far from perfect, but it is a start.

--- install-scripts/download-gpg-keys.sh
+++ install-scripts/download-gpg-keys.sh
@@ -8,6 +8,14 @@
 # - automatically refresh gpg keys: parcimonie
 # - randomize time between gpg key lookups: parcimonie
 # - use different Tor circuits for gpg key lookups: parcimonie
+# - might be better to add gpg key lookup to the relevant script,
+#   instead of centralized script
+
+# Variables
+MINTIME=60;
+MAXTIME=300;
+RANDOMTIME="$(shuf -i "$MINTIME"-"$MAXTIME" -n 1)";
+
 
 # Download gpg key Onion-node developer - Jules Mercier
 TRIES=0;
@@ -22,6 +30,10 @@ while [[ "$TRIES" -lt 10 ]]; do
     exit 0;
   fi
 done;
+
+# Sleep for 60-300 seconds between gpg key lookups
+echo "Sleeping for "$RANDOMTIME" seconds";
+sleep "$RANDOMTIME";
 
 # Download gpg key Bitcoin developer - Wladimir J. van der Laan
 TRIES=0;
