c61d52ca8210e32ccaf003b48e618651fdb57b96
Jules Mercier <jmercier@openmailbox.org>
2015-04-12 23:05:34 +0000

    Updated documentation in random-mac.sh

--- random-mac.sh
+++ random-mac.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -eu
+
 # TESTING code
 #
-# Created this because Raspberry pi wouldn't accept mac addresses from macchanger
-# but this seems to have the same issues when using it in /etc/network/interfaces file.
-# Prefer to use /etc/network/interfaces file, to keep network configuration at one point.
-# Need to troubleshoot the problem further/get another fix
+# Created this because Raspberry pi wouldn't accept mac addresses from the package macchanger.
+# This script sets a pseudo-random mac address.
+# It is used in combination with the /etc/network/interfaces file
+
+# To do
+# - Generate more random valid mac addresses by replacing the static '00' with pseudo-random code.
+#   But don't generate multicast and locally administered addresses.
 
 export RANDFILE=/etc/onion-node/.rnd;
 
@@ -14,6 +19,6 @@ MACEND=$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//');
 MACNEW="$MACSTART""$MACEND";
 
 ifconfig eth0 down hw ether "$MACNEW";
-# Sleep 10 to fix ifdown/ifup "RTNETLINK answers: Network is unreachable"
+# Sleep 10 to fix occasional ifdown/ifup "RTNETLINK answers: Network is unreachable" error.
 # In tests 1 second was enough, but use a margin of safety
 sleep 10;
