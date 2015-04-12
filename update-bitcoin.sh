749c0e1b853fb41d83f40c2b38ad90552ee94cc3
Jules Mercier <jmercier@openmailbox.org>
2015-04-12 01:35:27 +0000

    Created update-bitcoin.sh script
    What does update-bitcoin.sh do:
    - This script will be used to periodically check for new stable
      Bitcoin releases (disabled by default at this stage, see below).
    - New Bitcoin version information is provided by new Onion node
      releases/tags. Which will contain version numbers provided
      by Onion node developers (this setup has issues, see below).
    - The update-bitcoin.sh script will first check the PGP/GPG keys of
      the latest Onion-node release/tag and only continue if they are
      verified.
    - The script will then check its current checked out Onion node
      branch against the latest Onion node release/tag.
    - If the version numbers differ then an upgrade process will be
      started.
    - To compile the new Bitcoin version install-bitcoin.sh is used.
      Which will check the PGP/GPG keys of the latest Bitcoin release
      and only continue to build and install the latest Bitcoin version
      if they are verified.
    
    Implications:
    - Single point of failure: Onion node developers
      A malicious developer or compromised developer GPG key is
      pretty problematic.
    - Single point of failure: Github.com. What if github is compromised,
      unavailable or shutdown?
    
    Recommendations:
    - Since the goal is to create an unattended Bitcoin node. We have
      chosen to create this feature and want to enable it in a future
      release. We acknowledge that this will be a tradeoff
      between usability/availability and security.
    - Therefore we will recommend people to disable the auto update
      feature when they view this as to great a risk and install the
      updates manually.
    - Or leave the update feature on but regularly check the node/our
      website to view that everything is going hunky dory. Which is
      recommended anyway.
    
    Current state:
    - Right now automatic Bitcoin updates are disabled by default,
      the crontab is disabled.
    - We first want to do more testing before we make it the default.
    - We will notify Onion node users when we turn it on by default.
    
    Further thoughts on Bitcoin auto updates:
    - We would like another solution.
    - The problem is where we put the 'logic' to check for a
      new Bitcoin version. On the node itself? Or with the Onion node
      developers? Or somewhere else? We would like the intelligence
      on the node itself or in a decentralized setup.
    - We could go for the regular 'master' branch Bitcoin releases.
      But these are 'semi' stable at this point. And this might change
      in the future. Which is suboptimal.
    - We could go for the latest Bitcoin release/tag. But they include
      release candidates at this moment. And this might also change
      in the future. Which is suboptimal.
    - Alternatively a central location with the 'latest stable bitcoin
      version' managed by the Bitcoin developers would be an option.
      So the Onion node developers remove themselves from the update
      process. This will however shift the problem to the Bitcoin
      developers.

---
+++ update-bitcoin.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# This script will update the Onion node to the latest stable Bitcoin release
+# It will update at a random 1 to 10 day interval
+
+set -eu;
+
+# Variables
+ONIONDIR=/etc/onion-dir;
+BITCOININSTALL="$ONIONDIR"/install-scripts/install-bitcoin.sh;
+BITCOINSRC=/usr/local/src/bitcoin;
+LOCKDIR=/tmp/update-bitcoin.lock/;
+LOCKDIR2=/tmp/tor-bitcoin.lock/;
+# 86400 seconds is 1 days, 864000 seconds is 10 days
+MINTIME=86400;
+MAXTIME=864000;
+RANDOMTIME="$(shuf -i "$MINTIME"-"$MAXTIME" -n 1)";
+
+
+# Only run as root
+if [[ "$(id -u)" != "0" ]]; then
+  echo "ERROR: Must be run as root...exiting script";
+  exit 0;
+fi
+
+# Set lockfile/dir - mkdir is atomic
+# For portability flock or other Linux only tools are not used
+if mkdir "$LOCKDIR"; then
+  trap 'rmdir "$LOCKDIR"; exit' INT TERM EXIT; # remove LOCKDIR when script is interrupted, terminated or finished
+  echo "Successfully acquired lock on "$LOCKDIR"";
+else
+  echo "Failed to acquire lock on "$LOCKDIR"";
+  exit 0;
+fi
+
+# Sleep for 1-10 days
+echo "Sleeping for "$RANDOMTIME" seconds";
+sleep "$RANDOMTIME";
+
+# Check if other processes are running at this point
+# Like:
+# - re-installation of Onion node
+# - building packages from source
+# - Tor date check
+#
+# Check if a lockfile/LOCKDIR2 exists, wait max 2 hours
+TRIES=0
+while [[ -d "$LOCKDIR2" ]] && [[ "$TRIES" -lt 120 ]]; do
+  echo "Temporarily not able to acquire lock on "$LOCKDIR2"";
+  echo "Other processes might be running...retry in 60 seconds";
+  sleep 60;
+  TRIES=$(( $TRIES +1 ));
+done;
+
+# Set lockfile/dir - mkdir is atomic
+# For portability flock or other Linux only tools are not used
+if mkdir "$LOCKDIR2"; then
+  trap 'rmdir "$LOCKDIR2"; exit' INT TERM EXIT; # remove LOCKDIR2 when script is interrupted, terminated or finished
+  echo "Successfully acquired lock on "$LOCKDIR2"";
+else
+  echo "Failed to acquire lock on "$LOCKDIR2"";
+  exit 0;
+fi
+
+# Fetch latest Bitcoin updates
+cd "$BITCOINSRC";
+git fetch;
+git fetch --tags;
+
+# Fetch latest Onion updates
+cd "$ONIONDIR";
+git fetch;
+git fetch --tags;
+
+# Select latest Onion node release/tag
+LATESTTAG=$(git describe --tags $(git revc-list --tags --max-count=1));
+echo "Latest tag: "$LATESTTAG"";
+
+# Verify latest Onion node release/tag
+cd "$ONIONDIR";
+if git tag -v "$LATESTTAG" 2>&1 >> /dev/null | grep -q "^gpg: Good signature from" && ! git tag -v "$BITCOINVERSION" 2>&1 >> /dev/null | grep -q "^gpg: Bad signature from"; then
+  echo "Good GPG signature for latest Onion node release";
+else
+  echo "ERROR: Bad or missing GPG signature for latest Onion node release";
+  echo "Warning: The Bitcoin client will not be upgraded to a new version";
+  echo "The Bitcoin update script will exit now";
+  exit 0;
+fi
+
+# Check Bitcoin version between current branch/tag and the latest tag
+if git diff "$LATESTTAG" "$BITCOININSTALL" | grep -q "^[+-]BITCOINVERSION="; then
+  echo "A newer version of Bitcoin is available. This version will now be installed.";
+  echo "The update process might take 1 to 1.5 hours.";
+  git checkout "$LATESTTAG";
+  rmdir "$LOCKDIR2";
+  "$BITCOININSTALL";
+else
+  echo "Already running the latest bitcoin version";
+fi
