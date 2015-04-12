8965932534ee86860eb990739d262c02f3b63342
Jules Mercier <jmercier@openmailbox.org>
2015-04-12 03:37:24 +0000

    download-gpg-keys.sh - retry gpg cmd when it fails

--- install-scripts/download-gpg-keys.sh
+++ install-scripts/download-gpg-keys.sh
@@ -6,13 +6,31 @@
 # - check tlsdate gpg key
 # - check sks-keyservers.net key
 
-# gpg key onion-node developer Jules Mercier
-echo "Download gpg key onion-node developer Jules Mercier";
-gpg --recv-keys 4B8BBB5F5F2238A0BD72BE97F7698FEE3295ABB5
-echo "Successfully downloaded gpg key onion node developer - Jules Mercier";
 
-# gpg key bitcoin developer - Wladimir J. van der Laan
-echo "Download gpg key bitcoin developer - Wladimir J. van der Laan";
-gpg --recv-keys 71A3B16735405025D447E8F274810B012346C9A6
-echo "Succesfully downloaded gpg key bitcoin developer - Wladimir J. van der Laan";
+# Download gpg key Onion-node developer - Jules Mercier
+TRIES=0;
+while [[ "$TRIES" -lt 10 ]]; do
+  echo "Download gpg key onion-node developer Jules Mercier";
+  gpg --recv-keys 4B8BBB5F5F2238A0BD72BE97F7698FEE3295ABB5 && break;
+  sleep 30;
+  TRIES=$(( $TRIES +1 ));
+  if [[ "$TRIES" -eq 10 ]]; then
+    echo "ERROR: Downloading GPG key has failed.";
+    echo "The script will exit now.";
+    exit 0;
+  fi
+done;
 
+# Download gpg key Bitcoin developer - Wladimir J. van der Laan
+TRIES=0;
+while [[ "$TRIES" -lt 10 ]]; do
+  echo "Download gpg key bitcoin developer - Wladimir J. van der Laan";
+  gpg --recv-keys 71A3B16735405025D447E8F274810B012346C9A6 && break;
+  sleep 30;
+  TRIES=$(( $TRIES +1 ));
+  if [[ "$TRIES" -eq 10 ]]; then
+    echo "ERROR: Downloading GPG key has failed.";
+    echo "The script will exit now.";
+    exit 0;
+  fi
+done;
