6e096285288886e47712ff4004be38fe120c1e94
Jules Mercier <jmercier@openmailbox.org>
2015-04-12 02:41:14 +0000

    Fix added random-unattended-upgrades.sh to crontab
    Added random-unattended-upgrades.sh to install-crontabs.sh.
    Should have been there in release/tag 0.03.

--- install-scripts/install-crontabs.sh
+++ install-scripts/install-crontabs.sh
@@ -32,6 +32,8 @@ echo "# Check every 24 hours at random interval if time of Tor is accurate and s
 echo '0  0    * * *   root   /bin/sleep $(($RANDOM \% 1435))m; /etc/onion-node/tor-date-check.sh; /etc/onion-node/bitcoin-new-onion-address.sh' >> "$ONIONDIR"/crontab-tmp;
 echo "# Check every 24 hours at random interval if system time is accurate" >> "$ONIONDIR"/crontab-tmp;
 echo '0  0    * * *   root   /bin/sleep $(($RANDOM \% 1435))m; /etc/onion-node/tlsdate-script.sh' >> "$ONIONDIR"/crontab-tmp;
+echo "# Check for system updates at random interval every 0-5 days" >> "$ONIONDIR"/crontab-tmp;
+echo '0  0    * * *   root   /etc/onion-node/random-unattended-upgrades.sh' >> "$ONIONDIR"/crontab-tmp;
 echo "# Check every 30 minutes if Tor and bitcoind are running" >> "$ONIONDIR"/crontab-tmp;
 echo '*/30  *    * * *   root   /etc/onion-node/check-tor-bitcoin-running.sh' >> "$ONIONDIR"/crontab-tmp;
 echo "# DISABLED - Check for Bitcoin updates every 1-10 days" >> "$ONIONDIR"/crontab-tmp;
