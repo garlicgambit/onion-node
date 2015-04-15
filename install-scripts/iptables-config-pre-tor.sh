6276f484081a655517cda2f0f881d8e3beffabc5
Jules Mercier <jmercier@openmailbox.org>
2015-04-15 23:18:32 +0000

    iptables-config-pre-tor.sh changed VAR style

--- install-scripts/iptables-config-pre-tor.sh
+++ install-scripts/iptables-config-pre-tor.sh
@@ -18,69 +18,69 @@ ECHO=/bin/echo;
 LAN_INT=eth0;
 
 # Flush iptables chains
-"$ECHO" "Flush iptables chains";
-"$IPTABLES" -F;
-"$IPTABLES" -t nat -F;
-"$ECHO" "iptables flushed";
+"${ECHO}" "Flush iptables chains";
+"${IPTABLES}" -F;
+"${IPTABLES}" -t nat -F;
+"${ECHO}" "iptables flushed";
 
 # iptables default policies
-"$ECHO" "Loading iptables DROP policies";
-"$IPTABLES" -P INPUT DROP;
-"$IPTABLES" -P OUTPUT DROP;
-"$IPTABLES" -P FORWARD DROP;
-"$ECHO" "iptables DROP policies loaded";
+"${ECHO}" "Loading iptables DROP policies";
+"${IPTABLES}" -P INPUT DROP;
+"${IPTABLES}" -P OUTPUT DROP;
+"${IPTABLES}" -P FORWARD DROP;
+"${ECHO}" "iptables DROP policies loaded";
 
 
 # INPUT chain
-"$ECHO" "Loading INPUT chain";
+"${ECHO}" "Loading INPUT chain";
 
 # Drop invalid packets
-"$IPTABLES" -A INPUT -m state --state INVALID -j DROP;
+"${IPTABLES}" -A INPUT -m state --state INVALID -j DROP;
 
 # Drop invalid syn packets
-"$IPTABLES" -A INPUT -p tcp --tcp-flags ALL ACK,RST,SYN,FIN -j DROP;
-"$IPTABLES" -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP;
-"$IPTABLES" -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP;
+"${IPTABLES}" -A INPUT -p tcp --tcp-flags ALL ACK,RST,SYN,FIN -j DROP;
+"${IPTABLES}" -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP;
+"${IPTABLES}" -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP;
 
 # Drop incoming fragments
-"$IPTABLES" -A INPUT -f -j DROP;
+"${IPTABLES}" -A INPUT -f -j DROP;
 
 # Drop incoming xmas packets
-"$IPTABLES" -A INPUT -p tcp --tcp-flags ALL ALL -j DROP;
+"${IPTABLES}" -A INPUT -p tcp --tcp-flags ALL ALL -j DROP;
 
 # Drop incoming null packets
-"$IPTABLES" -A INPUT -p tcp --tcp-flags ALL NONE -j DROP;
+"${IPTABLES}" -A INPUT -p tcp --tcp-flags ALL NONE -j DROP;
 
 # Normal INPUT rules
-"$IPTABLES" -A INPUT -i lo -j ACCEPT;
-"$IPTABLES" -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
+"${IPTABLES}" -A INPUT -i lo -j ACCEPT;
+"${IPTABLES}" -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
 
 ### Logging is disabled to extend lifetime SD cards ###
-#"$IPTABLES" -A INPUT -j LOG;
-"$IPTABLES" -A INPUT -j DROP;
-"$ECHO" "INPUT chain loaded";
+#"${IPTABLES}" -A INPUT -j LOG;
+"${IPTABLES}" -A INPUT -j DROP;
+"${ECHO}" "INPUT chain loaded";
 
 
 # OUTPUT chain
-"$ECHO" "Loading OUTPUT chain";
+"${ECHO}" "Loading OUTPUT chain";
 
 # Drop invalid packets
-"$IPTABLES" -A OUTPUT -m state --state INVALID -j DROP;
+"${IPTABLES}" -A OUTPUT -m state --state INVALID -j DROP;
 
 # Prevent Tor transport data leaks
-"$IPTABLES" -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP;
-"$IPTABLES" -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP;
+"${IPTABLES}" -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,FIN ACK,FIN -j DROP;
+"${IPTABLES}" -A OUTPUT ! -o lo ! -d 127.0.0.1 ! -s 127.0.0.1 -p tcp -m tcp --tcp-flags ACK,RST ACK,RST -j DROP;
 
 # Normal OUTPUT rules
-"$IPTABLES" -A OUTPUT -o lo -j ACCEPT;
-"$IPTABLES" -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
-"$IPTABLES" -A OUTPUT -o "$LAN_INT" -p udp --dport 53 -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
-"$IPTABLES" -A OUTPUT -o "$LAN_INT" -p udp --dport 67 -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
-"$IPTABLES" -A OUTPUT -o "$LAN_INT" -p tcp --dport 80 --syn -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
+"${IPTABLES}" -A OUTPUT -o lo -j ACCEPT;
+"${IPTABLES}" -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT;
+"${IPTABLES}" -A OUTPUT -o "${LAN_INT}" -p udp --dport 53 -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
+"${IPTABLES}" -A OUTPUT -o "${LAN_INT}" -p udp --dport 67 -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
+"${IPTABLES}" -A OUTPUT -o "${LAN_INT}" -p tcp --dport 80 --syn -m state --state NEW -m owner --uid-owner 0 -j ACCEPT;
 
 ### Logging is disabled to extend lifetime SD cards ###
-#"$IPTABLES" -A OUTPUT -j LOG --log-uid;
-"$IPTABLES" -A OUTPUT -j DROP;
-"$ECHO" "OUTPUT chain loaded";
+#"${IPTABLES}" -A OUTPUT -j LOG --log-uid;
+"${IPTABLES}" -A OUTPUT -j DROP;
+"${ECHO}" "OUTPUT chain loaded";
 
-"$ECHO" "iptables script loaded";
+"${ECHO}" "iptables script loaded";
