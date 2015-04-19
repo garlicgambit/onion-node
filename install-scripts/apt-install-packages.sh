da3e45ec12d6aa0961c8a8b7da279c6e8c54f331
Jules Mercier <jmercier@openmailbox.org>
2015-04-19 00:22:40 +0000

    apt-install-packages.sh add structure script

--- install-scripts/apt-install-packages.sh
+++ install-scripts/apt-install-packages.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# Description:
+# Install general software packages for Onion node
+#
 
+# Bash options
 set -o errexit # exit script when a command fails
 set -o nounset # exit script when a variable is not set
+
 
 # Special APTPACKAGE install.sh check - this package is checked when running install.sh
 # This should be the first package to get installed.
