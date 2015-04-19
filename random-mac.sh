e74e32b0e398dd03e4aa3c485d19be5e2c4e55aa
Jules Mercier <jmercier@openmailbox.org>
2015-04-19 00:02:57 +0000

    random-mac.sh add structure script
    Structure:
    - Documentation/comments
    - Bash options
    - Variables
    - Commands

--- random-mac.sh
+++ random-mac.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-
-
-# TESTING code
 #
-# Created this because Raspberry pi wouldn't accept mac addresses from the package macchanger.
+# Description:
+# TESTING code
+# Created this because Raspberry pi doesn't accept mac addresses from the package macchanger.
 # This script sets a pseudo-random mac address.
 # It is used in combination with the /etc/network/interfaces file
-
-# To do
+#
+# TODO:
 # - Generate more random valid mac addresses by replacing the static '00' with pseudo-random code.
 #   But don't generate multicast and locally administered addresses.
+#
 
+# Bash options
 set -o errexit # exit script when a command fails
 set -o nounset # exit script when a variable is not set
 
+
+# Variables
 export RANDFILE=/etc/onion-node/.rnd;
 
 readonly MAC_START=00:;
 readonly MAC_END=$(openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//');
 readonly MAC_NEW="${MAC_START}""${MAC_END}";
 
+
+# Set new mac address
 ifconfig eth0 down hw ether "${MAC_NEW}";
+
 # Sleep 10 to fix occasional ifdown/ifup "RTNETLINK answers: Network is unreachable" error.
 # In tests 1 second was enough, but use a margin of safety
 sleep 10;
