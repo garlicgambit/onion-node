002fe27ec992f0f0069cd08bfa50689dc6bc7980
Jules Mercier <jmercier@openmailbox.org>
2015-04-29 00:55:07 +0000

    apt-install-packages.sh add '|| true' for set -e

--- install-scripts/apt-install-packages.sh
+++ install-scripts/apt-install-packages.sh
@@ -11,13 +11,13 @@ set -o nounset # exit script when a variable is not set
 
 # Special APTPACKAGE install.sh check - this package is checked when running install.sh
 # This should be the first package to get installed.
-apt-get install -y macchanger
+apt-get install -y macchanger || true
 
 # Install other software
-apt-get install -y gnupg-curl unattended-upgrades
+apt-get install -y gnupg-curl unattended-upgrades || true
 
 # Install bitcoin dependencies
-apt-get install -y autoconf build-essential libboost-chrono-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libssl-dev
+apt-get install -y autoconf build-essential libboost-chrono-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libtool libssl-dev || true
 
 # Install tlsdate dependencies
-apt-get install -y autoconf autotools-dev build-essential fakeroot libevent-dev libssl-dev libtool pkg-config
+apt-get install -y autoconf autotools-dev build-essential fakeroot libevent-dev libssl-dev libtool pkg-config || true
