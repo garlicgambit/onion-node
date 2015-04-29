959fbfb13aa7bb03081e1b963ace20a90248aada
Jules Mercier <jmercier@openmailbox.org>
2015-04-29 00:58:14 +0000

    apt-remove.sh add '|| true' for set -e

--- install-scripts/apt-remove.sh
+++ install-scripts/apt-remove.sh
@@ -10,11 +10,11 @@ set -o nounset # exit script when a variable is not set
 
 
 # General software cleanup
-apt-get purge -y cifs-utils cups-bsd cups-client cups-common desktop-base dillo epiphany-browser epiphany-browser-data galculator gnome-icon-theme gnome-themes-standard gnome-themes-standard-data gpicview gstreamer1.0-* iputils-ping jackd jackd2 java-common krb5-locales lightdm lightdm-gtk-greeter lxappearance lxde lxde-common lxde-icon-theme lxinput lxmenu-data lxpolkit lxrandr lxsession lxsession-edit lxshortcut lxterminal minecraft-pi netcat-openbsd netcat-traditional nfs-common ntp omxplayer openbox openssh-client openssh-server oracle-java8-jdk penguinspuzzle pistore python-minecraftpi python-picamera python3-picamera raspberrypi-artwork rpcbind rsync ruby1.9.1 samba-common smartsim squeak-vm strace supercollider-common tcpd timidity traceroute tsconf wireless-tools wolfram-engine wpagui xpdf xserver-common
+apt-get purge -y cifs-utils cups-bsd cups-client cups-common desktop-base dillo epiphany-browser epiphany-browser-data galculator gnome-icon-theme gnome-themes-standard gnome-themes-standard-data gpicview gstreamer1.0-* iputils-ping jackd jackd2 java-common krb5-locales lightdm lightdm-gtk-greeter lxappearance lxde lxde-common lxde-icon-theme lxinput lxmenu-data lxpolkit lxrandr lxsession lxsession-edit lxshortcut lxterminal minecraft-pi netcat-openbsd netcat-traditional nfs-common ntp omxplayer openbox openssh-client openssh-server oracle-java8-jdk penguinspuzzle pistore python-minecraftpi python-picamera python3-picamera raspberrypi-artwork rpcbind rsync ruby1.9.1 samba-common smartsim squeak-vm strace supercollider-common tcpd timidity traceroute tsconf wireless-tools wolfram-engine wpagui xpdf xserver-common || true
 
 # Cleanup network drivers 
-apt-get purge -y firmware-atheros firmware-brcm80211 firmware-libertas firmware-ralink firmware-realtek
+apt-get purge -y firmware-atheros firmware-brcm80211 firmware-libertas firmware-ralink firmware-realtek || true
 
 # Cleanup leftovers
-apt-get autoremove -y
-apt-get autoclean -y
+apt-get autoremove -y || true
+apt-get autoclean -y || true
